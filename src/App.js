00e3caad2367687af96646b08e25b6be63ed7383
Wollivan <tim.smith2814@gmail.com>
2022-04-21 16:08:54 +0100

    Add hidden images to use in the github readme

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import Instructions from "./Components/Instructions/Instructions";
 import PageHeader from "./Components/PageHeader/PageHeader";
 import Printing from "./Components/Printing/Printing";
 import PageFooter from "./Components/PageFooter/PageFooter";
+import GameplayImage from "./assets/images/gameplay.jpg";
+import SCADImage from "./assets/images/printing-assets.png";
+import Logo from "./assets/images/aeronauts-logo.png";
+import MovementImage from "./assets/images/movement-example.png";
 
 function App() {
   return (
@@ -11,6 +15,26 @@ function App() {
       <Instructions />
       <Printing />
       <PageFooter />
+      <img
+        className="hidden-image"
+        src={GameplayImage}
+        alt="hidden img, you shouldn't see this ever, its just for the readme"
+      />
+      <img
+        className="hidden-image"
+        src={SCADImage}
+        alt="hidden img, you shouldn't see this ever, its just for the readme"
+      />
+      <img
+        className="hidden-image"
+        src={Logo}
+        alt="hidden img, you shouldn't see this ever, its just for the readme"
+      />
+      <img
+        className="hidden-image"
+        src={MovementImage}
+        alt="hidden img, you shouldn't see this ever, its just for the readme"
+      />
     </div>
   );
 }
